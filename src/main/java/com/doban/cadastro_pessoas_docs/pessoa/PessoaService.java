aef909b2c9dff40558360ae07b1be025c2ec55c2
Vinicius <vpburci@gmail.com>
2025-10-07 11:20:48 -0300

    Revert "pessoaDTO separado do import excel"
    This reverts commit e177bc40267c26168e1ada815c06ecc3af578aae.

--- src/main/java/com/doban/cadastro_pessoas_docs/pessoa/PessoaService.java
+++ src/main/java/com/doban/cadastro_pessoas_docs/pessoa/PessoaService.java
@@ -1,14 +1,8 @@
 package com.doban.cadastro_pessoas_docs.pessoa;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class PessoaService {
-
-    public List<PessoaDTO> buscarTodasPessoas() {
-        return null; // Retorne a lista de pessoas
-    }
 
 }
