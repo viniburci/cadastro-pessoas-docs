e177bc40267c26168e1ada815c06ecc3af578aae
Vinicius <vpburci@gmail.com>
2025-10-07 11:19:34 -0300

    pessoaDTO separado do import excel

--- src/main/java/com/doban/cadastro_pessoas_docs/pessoa/PessoaService.java
+++ src/main/java/com/doban/cadastro_pessoas_docs/pessoa/PessoaService.java
@@ -1,8 +1,14 @@
 package com.doban.cadastro_pessoas_docs.pessoa;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class PessoaService {
+
+    public List<PessoaDTO> buscarTodasPessoas() {
+        return null; // Retorne a lista de pessoas
+    }
 
 }
