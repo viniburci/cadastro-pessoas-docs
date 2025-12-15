968970a922b23d9c9f8f8547436c0a171f2132b1
Vinicius <vpburci@gmail.com>
2025-12-15 12:09:57 -0300

    adicionando buscarPorPessoaId em RecursoRocadeiraRepository

--- src/main/java/com/doban/cadastro_pessoas_docs/recurso/recurso_rocadeira/RecursoRocadeiraRepository.java
+++ src/main/java/com/doban/cadastro_pessoas_docs/recurso/recurso_rocadeira/RecursoRocadeiraRepository.java
@@ -1,7 +1,9 @@
 package com.doban.cadastro_pessoas_docs.recurso.recurso_rocadeira;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecursoRocadeiraRepository extends JpaRepository<RecursoRocadeira, Long> {
-    
+    List<RecursoRocadeira> findByPessoaId(Long pessoaId);
 }
