870614a4bf99c8b2bc413b45d57f21177dba8cb2
Vinicius <vpburci@gmail.com>
2025-12-16 15:56:11 -0300

    adicionando request e reponse DTOs de Rocadeira

---
+++ src/main/java/com/doban/cadastro_pessoas_docs/rocadeira/RocadeiraRequestDTO.java
@@ -0,0 +1,15 @@
+package com.doban.cadastro_pessoas_docs.rocadeira;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RocadeiraRequestDTO {
+
+    private String marca;
+    private String numeroSerie;
+    
+}
---
+++ src/main/java/com/doban/cadastro_pessoas_docs/rocadeira/RocadeiraResponseDTO.java
@@ -0,0 +1,22 @@
+package com.doban.cadastro_pessoas_docs.rocadeira;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RocadeiraResponseDTO {
+
+    private Long id;
+    private String marca;
+    private String numeroSerie;
+
+    public RocadeiraResponseDTO(Rocadeira rocadeira) {
+        this.id = rocadeira.getId();
+        this.marca = rocadeira.getMarca();
+        this.numeroSerie = rocadeira.getNumeroSerie();
+    }
+
+}
