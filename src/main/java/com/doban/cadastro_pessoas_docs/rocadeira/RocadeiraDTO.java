d68bc4d38c898c52ddacb1eb4e4d6f7cbcb25fb1
Vinicius <vpburci@gmail.com>
2025-12-16 14:52:34 -0300

    desenvolvendo RocadeiraDTO

---
+++ src/main/java/com/doban/cadastro_pessoas_docs/rocadeira/RocadeiraDTO.java
@@ -0,0 +1,30 @@
+package com.doban.cadastro_pessoas_docs.rocadeira;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RocadeiraDTO {
+
+    private String marca;
+    private String NumeroSerie;
+
+    public RocadeiraDTO() {
+    }
+
+    public RocadeiraDTO(String marca, String numeroSerie) {
+        this.marca = marca;
+        this.NumeroSerie = numeroSerie;
+    }
+
+    public RocadeiraDTO(RocadeiraDTO rocadeira) {
+        this.marca = rocadeira.getMarca();
+        this.NumeroSerie = rocadeira.getNumeroSerie();
+    }
+
+    public Rocadeira toEntity() {
+        return new Rocadeira(
+                null, this.marca, this.NumeroSerie);
+    }
+}
