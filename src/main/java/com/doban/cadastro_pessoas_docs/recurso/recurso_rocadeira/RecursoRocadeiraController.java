ecd5e296d2a914569f643bf7a5b216284e4b1c6a
Vinicius <vpburci@gmail.com>
2026-01-05 11:00:38 -0300

    alterando endpoint de devolução para PUT e adicionando @RequestBody em RecursoRocadeiraController

--- src/main/java/com/doban/cadastro_pessoas_docs/recurso/recurso_rocadeira/RecursoRocadeiraController.java
+++ src/main/java/com/doban/cadastro_pessoas_docs/recurso/recurso_rocadeira/RecursoRocadeiraController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,8 +44,8 @@ public class RecursoRocadeiraController {
         return ResponseEntity.ok().body(service.criar(dto));
     }
 
-    @PostMapping(path = "/{id}/devolucao")
-    public ResponseEntity<RecursoRocadeiraResponseDTO> registrarDevolucao(@PathVariable Long id, DevolucaoDTO dto) {
+    @PutMapping("/{id}/devolucao")
+    public ResponseEntity<RecursoRocadeiraResponseDTO> registrarDevolucao(@PathVariable Long id, @RequestBody DevolucaoDTO dto) {
         return ResponseEntity.ok().body(service.registrarDevolucao(id, dto));
     }
     
