d2846c3d44b94fb8762f5da2b7cb0364d4de91b6
Vinicius <vpburci@gmail.com>
2026-01-05 12:26:07 -0300

    retirando metodo desnecessario de PessoaRepository

--- src/main/java/com/doban/cadastro_pessoas_docs/domain/pessoa/PessoaRepository.java
+++ src/main/java/com/doban/cadastro_pessoas_docs/domain/pessoa/PessoaRepository.java
@@ -3,7 +3,6 @@ package com.doban.cadastro_pessoas_docs.domain.pessoa;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 
@@ -12,6 +11,5 @@ public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
 	
 	Optional<Pessoa> findByCpf(String cpf);
 
-	boolean existsById(@NonNull Long id);
 
 }
